8fbbfda10419363ac8c4fcbeeb795b174053bb09
eun-young <eunyoung12@kaist.ac.kr>
2024-07-06 16:58:29 +0900

    <feat> 카카오 로그인 구현

---
+++ app/src/main/java/com/example/week2/NextPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.week2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NextPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.next_page)
+    }
+}
