233ab4d7ab35f20bf0766973c6a5140135989e28
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-07 01:42:35 +0900

    WordListAdapter

--- app/src/main/java/com/example/week2/MealActivity.kt
+++ app/src/main/java/com/example/week2/MealActivity.kt
@@ -3,6 +3,8 @@ package com.example.week2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MealActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,11 @@ class MealActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         //supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
     override fun onSupportNavigateUp(): Boolean {
