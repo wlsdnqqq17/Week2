b051a681b140fe04058bb2b871fc54cc8c03024b
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-07 17:56:22 +0900

    🔄 refactor: rename activity_meal.xml to activity_potato.xml

--- app/src/main/java/com/example/week2/Activity/PotatoActivity.kt
+++ app/src/main/java/com/example/week2/Activity/PotatoActivity.kt
@@ -1,10 +1,26 @@
 package com.example.week2
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PotatoActivity : AppCompatActivity() {
+
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+
+    private lateinit var newWordActivityLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_potato)
@@ -14,9 +30,40 @@ class PotatoActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@PotatoActivity, NewWordActivity::class.java)
+            newWordActivityLauncher.launch(intent)
+        }
+
+        newWordActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let { reply ->
+                    val word = Word(reply)
+                    wordViewModel.insert(word)
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        wordViewModel.allWords.observe(this) { words ->
+            words.let { adapter.submitList(it) }
+        }
     }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
