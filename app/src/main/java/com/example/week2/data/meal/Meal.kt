369dafa8f1938a506c9ac58b62e622ae85fe7555
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-08 15:16:12 +0900

    ✨ Meal.kt, MealDao.kt

--- app/src/main/java/com/example/week2/data/meal/Meal.kt
+++ app/src/main/java/com/example/week2/data/meal/Meal.kt
@@ -5,14 +5,12 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-
-
 @Entity(tableName = "meal_table", indices = [Index(value = ["date"])])
 data class Meal(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "meal_time") val mealTime: String,
     @ColumnInfo(name = "meal_name") val mealName: String,
     @ColumnInfo(name = "price") val price: Int,
-    @ColumnInfo(name = "date") val date: String,
-    @ColumnInfo(name = "memo") val memo: String?
+    @ColumnInfo(name = "date") val date: Long,
+    @ColumnInfo(name = "memo") val memo: String? = null
 )
--- app/src/main/java/com/example/week2/data/meal/MealDao.kt
+++ app/src/main/java/com/example/week2/data/meal/MealDao.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MealDao {
     @Query("SELECT * FROM meal_table WHERE date = :date")
-    fun getMealsByDate(date: String): Flow<List<Meal>>
+    fun getMealsByDate(date: Long): Flow<List<Meal>>
 
     @Insert
     suspend fun insert(meal: Meal)
@@ -20,4 +20,7 @@ interface MealDao {
 
     @Delete
     suspend fun delete(meal: Meal)
+
+    @Query("SELECT SUM(price) FROM meal_table WHERE date BETWEEN :startOfDay AND :endOfDay")
+    fun getTodayMealCostSum(startOfDay: Long, endOfDay: Long): Flow<Float>
 }
