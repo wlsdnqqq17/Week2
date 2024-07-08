52308664922a6eaa91d6234d129379587ff5a393
eun-young <eunyoung12@kaist.ac.kr>
2024-07-08 19:39:28 +0900

    <feat> 친구검색 기능 구현 친구추가를 위한 친구검색 기능을 DB의 login_id를 이용해 검색하도록 구현

---
+++ app/src/main/java/com/example/week2/ui/activity/FriendAddActivity.kt
@@ -0,0 +1,60 @@
+package com.example.week2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.kakao.sdk.user.UserApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FriendAddActivity : AppCompatActivity() {
+    private lateinit var apiService: ApiService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_add)
+
+        apiService = RetrofitClient.getInstance().create(ApiService::class.java)
+
+        val toolbar: Toolbar = findViewById(R.id.friend_search_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val searchEditText: EditText = findViewById(R.id.friend_search_EditText)
+        val searchButton: Button = findViewById(R.id.Button)
+        searchButton.setOnClickListener {
+            val userId = searchEditText.text.toString()
+            if (userId.isNotEmpty()) {
+                searchUser(userId)
+            } else {
+                Toast.makeText(this, "사용자 ID를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
+    }
+
+    private fun searchUser(userId: String) {
+        apiService.searchUser(userId).enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful && response.body()?.success == true) {
+                    Toast.makeText(this@FriendAddActivity, "OK", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@FriendAddActivity, "사용자를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                Toast.makeText(this@FriendAddActivity, "검색 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
