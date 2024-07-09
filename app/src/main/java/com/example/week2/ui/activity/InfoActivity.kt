8b38cb8dc04737bfcd212b005f445570d1e120f0
eun-young <eunyoung12@kaist.ac.kr>
2024-07-09 22:55:11 +0900

    <feat> 로그아웃을 하면 RoomDB와 SharedPreference 초기화

--- app/src/main/java/com/example/week2/ui/activity/InfoActivity.kt
+++ app/src/main/java/com/example/week2/ui/activity/InfoActivity.kt
@@ -8,12 +8,22 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.week2.data.AppRoomDatabase
+import com.example.week2.data.item.ItemRepository
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class InfoActivity : AppCompatActivity() {
+    private lateinit var repository: ItemRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val db = AppRoomDatabase.getDatabase(applicationContext, CoroutineScope(Dispatchers.IO))
+        repository = ItemRepository(db.itemDao())
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -50,7 +60,12 @@ class InfoActivity : AppCompatActivity() {
                     editor.clear()
                     editor.apply()
 
-                    val intent = Intent(this, MainActivity::class.java)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        repository.deleteAll() // RoomDB 초기화
+                        Log.d("Logout", "All items deleted from the local database")
+                    }
+
+                    val intent = Intent(this@InfoActivity, MainActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(intent)
                     finish()
@@ -61,5 +76,22 @@ class InfoActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
+    }
+
+    private fun logout() {
+        CoroutineScope(Dispatchers.IO).launch {
+            repository.deleteAll() // RoomDB 초기화
+            Log.d("Logout", "All items deleted from the local database")
+        }
+
+        val sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            clear() // SharedPreferences 초기화
+            apply()
+        }
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
