f224a23183d1d288a285bd56ca645a22ff709120
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-10 20:14:07 +0900

    <release> delete deleteDB

--- app/src/main/java/com/example/week2/data/AppRoomDatabase.kt
+++ app/src/main/java/com/example/week2/data/AppRoomDatabase.kt
@@ -31,7 +31,7 @@ abstract class AppRoomDatabase : RoomDatabase() {
         ): AppRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 //개발용 임~~~~
-                context.deleteDatabase("app_database")
+                //context.deleteDatabase("app_database")
                 //개발용 임~~~~
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
@@ -60,33 +60,6 @@ abstract class AppRoomDatabase : RoomDatabase() {
         }
 
         suspend fun populateDatabase(mealDao: MealDao, itemDao: ItemDao) {
-            val meal = Meal( mealTime = "점심", mealName = "피자", price = 5000, date = "2024-07-10", memo = "")
-            mealDao.insert(meal)
-            val item1 = Item(
-                id = 1,
-                name = "naked",
-                category = "clothes",
-                item_image_url = "http://ec2-13-124-112-168.ap-northeast-2.compute.amazonaws.com/MyAvatar_item/Naked.png",
-                price = 2,
-                isPurchased = true)
-            val item2 = Item(
-                id = 3,
-                name = "haksa",
-                category = "hat",
-                item_image_url = "http://ec2-13-124-112-168.ap-northeast-2.compute.amazonaws.com/MyAvatar_item/haksa.png",
-                price = 1,
-                isPurchased = true)
-            val item3 = Item(
-                id = 8,
-                name = "butterfly",
-                category = "accessory",
-                item_image_url = "http://ec2-13-124-112-168.ap-northeast-2.compute.amazonaws.com/MyAvatar_item/butterfly.png",
-                price = 2,
-                isPurchased = true)
-            itemDao.insert(item1)
-            itemDao.insert(item2)
-            itemDao.insert(item3)
-
         }
     }
 }
